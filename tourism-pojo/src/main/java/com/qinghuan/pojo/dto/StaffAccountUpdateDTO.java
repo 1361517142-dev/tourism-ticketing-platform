ec7026020cd47fbf3c1828a6dd92ca43b29f1dd8
qinghuan136 <17287118+qinghuan136@user.noreply.gitee.com>
2026-07-29 23:07:52 +0800

    完成了运营端工作人员管理模块

---
+++ tourism-pojo/src/main/java/com/qinghuan/pojo/dto/StaffAccountUpdateDTO.java
@@ -0,0 +1,24 @@
+package com.qinghuan.pojo.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 运营者修改工作人员基本资料的请求参数。
+ * 登录账号、密码、状态和所属景点由各自的业务接口处理。
+ */
+@Getter
+@Setter
+public class StaffAccountUpdateDTO {
+
+    @NotBlank(message = "工作人员姓名不能为空")
+    @Size(max = 50, message = "工作人员姓名不能超过50个字符")
+    private String displayName;
+
+    @NotBlank(message = "手机号不能为空")
+    @Pattern(regexp = "^1\\d{10}$", message = "手机号格式不正确")
+    private String phone;
+}
